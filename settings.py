0dd856f0ef020ab5a41c6cafd8680dc242e232d7
Eduardo Sebastian <esebastian@altiora.es>
2019-01-14 11:23:36 +0100

    Fix featured programmes list

--- settings.py
+++ settings.py
@@ -131,7 +131,7 @@ COOKIES_URL             = 'http://www.montmelo.cat/lnk19/avis-legal.htm#Cookies'
 # ----------------------
 
 # Programmes to feature as example in home page.
-FEATURED_PROGRAMMES = ['1621', '1710', '3380', '2410', '3110']
+FEATURED_PROGRAMMES = ['1620', '1710', '3380', '2410', '3110']
 
 # Number of programmes to feature in home page. Default: 3
 # NUMBER_OF_FEATURED_PROGRAMMES = 3
